826491ee895e66968ca75b1ae001921a9fa505dd
Mor Gazith <mor.gazith@benchmarkemail.com>
2023-03-02 15:49:56 +0200

    CON-1564 / Remove ahead-of-time flag from all in-use FullContact libraries

--- project.clj
+++ project.clj
@@ -1,16 +1,20 @@
-(defproject fullcontact/full.db "1.0.6-SNAPSHOT"
+(defproject fullcontact/full.db "1.0.7"
   :description "DB sugar (Korma + hikariCP + core.async)."
-  :url "https://github.com/fullcontact/full.db"
+  :url "https://github.com/contactsplusapp/full.db"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-  :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :repositories [["fullcontact" {:url "https://contactsplus.jfrog.io/artifactory/repo"}]
+                 ["releases" {:url "https://contactsplus.jfrog.io/artifactory/libs-release-local"}]
+                 ["snapshots" {:url "https://contactsplus.jfrog.io/artifactory/libs-snapshot-local"}]]
+  :deploy-repositories [["releases" {:url "https://contactsplus.jfrog.io/artifactory/libs-release-local" :sign-releases false}]
+                        ["snapshots" {:url "https://contactsplus.jfrog.io/artifactory/libs-snapshot-local" :sign-releases false}]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [korma "0.4.3"]
                  [hikari-cp "1.8.0"]
-                 [fullcontact/full.core "1.0.4" :exclusions [org.clojure/clojurescript]]
-                 [fullcontact/full.metrics "0.11.4"]
-                 [fullcontact/full.async "1.0.0"]
+                 [fullcontact/full.core "1.1.3" :exclusions [org.clojure/clojurescript]]
+                 [fullcontact/full.metrics "0.13.1"]
+                 [fullcontact/full.async "1.1.1"]
                  [org.liquibase/liquibase-core "3.5.3"]
                  [com.mattbertolini/liquibase-slf4j "2.0.0"
                   :exclusions [org.slf4j/slf4j-api org.yaml/snakeyaml]]]
@@ -21,5 +25,4 @@
                   ["deploy"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
-                  ["vcs" "push"]]
-  :aot :all)
+                  ["vcs" "push"]])
